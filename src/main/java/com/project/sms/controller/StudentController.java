105c85dc80d6ca66adb0114f334480eab8b7af67
davi38 <davi4alves@gmail.com>
2022-10-27 04:03:49 -0300

    feat: new route with html

--- src/main/java/com/project/sms/controller/StudentController.java
+++ src/main/java/com/project/sms/controller/StudentController.java
@@ -1,18 +1,27 @@
 package com.project.sms.controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.project.sms.model.Student;
+import com.project.sms.service.StudentService;
 
 @Controller
 public class StudentController {
 
-	@GetMapping
-	List<Student> getAllStudents() {
-		
+	private StudentService studentService;
+
+	public StudentController(StudentService studentService) {
+		super();
+		this.studentService = studentService;
 	}
-	
+
+	// handler method to handle list students and return mode and view
+
+	@GetMapping("/students")
+	public String listStudents(Model model) {
+		model.addAttribute("students", studentService.getAllStudents());
+		return "students";
+	}
+
 }
--- src/main/java/com/project/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/project/sms/service/impl/StudentServiceImpl.java
@@ -2,14 +2,27 @@ package com.project.sms.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.project.sms.model.Student;
+import com.project.sms.repository.StudentRepository;
 import com.project.sms.service.StudentService;
 
+@Service
 public class StudentServiceImpl implements StudentService {
+
+	private StudentRepository studentRepository;
+	
+	public StudentServiceImpl(StudentRepository studentRepository) {
+		super();
+		this.studentRepository = studentRepository;
+	}
+
+
 
 	@Override
 	public List<Student> getAllStudents(){
-		
+		return studentRepository.findAll();
 	}
 	
 }
