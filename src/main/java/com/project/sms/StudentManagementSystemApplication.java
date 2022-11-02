8713ec1abc058b4120a6cc07baaffc6f05097a07
d1av <davi4alves@gmail.com>
2022-11-02 19:30:19 -0300

    feat: student database

--- src/main/java/com/project/sms/StudentManagementSystemApplication.java
+++ src/main/java/com/project/sms/StudentManagementSystemApplication.java
@@ -1,13 +1,31 @@
 package com.project.sms;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.project.sms.model.Student;
+import com.project.sms.repository.StudentRepository;
+
 @SpringBootApplication
-public class StudentManagementSystemApplication {
+public class StudentManagementSystemApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementSystemApplication.class, args);
 	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Student student1 = new Student(null, "Ramesh", "Fadatare","ramesh@gmail.com");
+		studentRepository.save(student1);
+	
+		Student student2 = new Student(null,"Sanjay", "Jadhav","sanjay@gmail.com");
+		studentRepository.save(student2);
+	}
+
 
 }
